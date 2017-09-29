526b7985c0bdce04a9f77a930acdd6589c20b632
vovabatsyk <vovabatsyk@gmail.com>
2017-09-30 02:27:57 +0300

    git\Initials

--- js/index.js
+++ js/index.js
@@ -6,3 +6,40 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+$(function() {
+    
+        $('form input[type=submit]').click(function(e) {
+            const form = $('form')[0];
+            if (!form.checkValidity()) {
+                return;
+            }
+            e.preventDefault();
+            $.ajax({
+                url: "https://formspree.io/vovabatsyk@gmail.com",
+                method: "POST",
+                data: { 
+                    clientName: $('#client-name').val(),
+                    clientEemail: $('#client-email').val(),
+                    clientMessage: $('#client-message').val()},
+                dataType: "json"
+            }).done(function() {
+                form.reset();
+                $('#message-box').html('<h2>Thank you!<h2>');
+            }).fail(function() {
+                $('#message-box').html('Sorry, error occurred!');
+            });
+        });
+    
+    });
+    
+$(document).ready(function() {
+        function blink(){
+            $('#message-box')
+            .fadeOut('fast')
+            .fadeIn('fast');
+        
+            setTimeout(blink,100);
+        }
+        blink();      
+});
