1e95fa63fa8a25ea9b40fe0f84e6a4f51b9ef8d1
vovabatsyk <vovabatsyk@gmail.com>
2017-10-04 02:34:36 +0300

    git\Initials

--- js/index.js
+++ js/index.js
@@ -1,45 +1,49 @@
-function myFunction() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 $(function() {
-    
-        $('form input[type=submit]').click(function(e) {
-            const form = $('form')[0];
-            if (!form.checkValidity()) {
-                return;
-            }
-            e.preventDefault();
-            $.ajax({
-                url: "https://formspree.io/vovabatsyk@gmail.com",
-                method: "POST",
-                data: { 
-                    clientName: $('#client-name').val(),
-                    clientEemail: $('#client-email').val(),
-                    clientMessage: $('#client-message').val()},
-                dataType: "json"
-            }).done(function() {
-                form.reset();
-                $('#message-box').html('<h2>Thank you!<h2>');
-            }).fail(function() {
-                $('#message-box').html('Sorry, error occurred!');
-            });
-        });
-    
-    });
-    
-$(document).ready(function() {
-        function blink(){
-            $('#message-box')
-            .fadeOut('fast')
-            .fadeIn('fast');
-        
-            setTimeout(blink,100);
+    $('header .icon').click(function() {
+        let x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
         }
-        blink();      
+    });
+
+    $('form input[type=submit]').click(function(e) {
+        const form = $('form')[0];
+        if (!form.checkValidity()) {
+            return;
+        }
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/vovabatsyk@gmail.com",
+            method: "POST",
+            data: {
+                clientName: $('#client-name').val(),
+                clientEemail: $('#client-email').val(),
+                clientMessage: $('#client-message').val()
+            },
+            dataType: "json"
+        }).done(function() {
+            form.reset();
+            $('#message-box').html('<h2>Thank you!<h2>');
+        }).fail(function() {
+            $('#message-box').html('Sorry, error occurred!');
+        });
+    });
+
+    function blink() {
+        $('#message-box').fadeOut('fast').fadeIn('fast')
+        setTimeout(blink, 100);
+    }
+    blink();
+
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
 });
